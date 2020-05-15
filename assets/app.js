2f73a7a3a489c613531bdfb392e2764e5142911e
Code-Tim <62041831+Code-Tim@users.noreply.github.com>
2020-05-14 21:10:52 -0400

    Added jquerry to main and moved jquerry to the head on login

--- assets/app.js
+++ assets/app.js
@@ -4,7 +4,7 @@ $.ajax({
   url: "https://www.googleapis.com/youtube/v3/search",
   data: {
     key: "AIzaSyCFonS58Mi9FXxIvqe0p4YY1Rf8HVhcAIg",
-    q: "Metallica",
+    q: searchResults,
     part: "snippet",
     maxResults: 10,
     type: "video",
@@ -13,6 +13,15 @@ $.ajax({
 }).then((response) => {
   console.log(response);
 });
+
+//write a function to show 10 video results from youtube API on 
+
+//write a function to show video results from youtube API
+function displayvideo() {
+  let searchResulsts = localStorage.getItem('songName')
+
+
+}
 
 //to play video go to youtube.com/watch?v=<videoid>
 
@@ -40,7 +49,7 @@ $.ajax({
   data: {
     apikey: "AMlA6dh5sfwIqUjSn26jTvgrF6xaX92f",
     keyword: "metallica",
-    
+
   },
 }).then((response) => {
   console.log(response);
