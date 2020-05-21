2bfe736cad28a50b2fc96795656a8339d504bdc4
Code-Tim <62041831+Code-Tim@users.noreply.github.com>
2020-05-21 19:30:45 -0400

    Changed URL's from absolute to relative

--- assets/index.js
+++ assets/index.js
@@ -3,13 +3,15 @@ $(document).ready(function () {
 
   if (token) {
     sessionStorage.setItem("access_token", token);
-    window.location = "http://127.0.0.1:5500/search.html";
+    // window.location = "http://127.0.0.1:5500/search.html";
+    window.location = "/search.html";
   }
 
   const base_url = "https://accounts.spotify.com/authorize?";
   const params = {
     client_id: "7c62586e78b4439e82cba40ac1d1fc4f",
-    redirect_uri: "http://127.0.0.1:5500/index.html",
+    // redirect_uri: "http://127.0.0.1:5500/index.html",
+    redirect_uri: "/index.html",
     response_type: "token",
   };
 
